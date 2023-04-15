9428e268f6f5c93142341add5af1a0a9952fdd82
default-kaas <dogen1996@live.nl>
2023-04-15 17:42:45 +0200

    Implemented OR and AND condition for friend filtering

--- app.vue
+++ app.vue
@@ -11,7 +11,17 @@
       </template>
     </select>
     <div>
-    <div class="flex mx-2">
+    <div class="flex mx-2 h-12">
+      <div class="flex items-center" v-if="selectedFriends.size > 0">
+        <select v-model="selectedFilterType" >
+          <option default class="flex items-center py-1 px-2 bg-gray-800 text-white" :value="'OR'">
+            OR
+          </option>
+          <option class="flex items-center py-1 px-2 bg-gray-800 text-white" :value="'AND'">
+            AND
+          </option>
+        </select>
+      </div>
       <FriendThumbnail v-for="friend in selectedFriends" :user="friend" @delete="removeSelectedFriend" />
     </div>
     </div>
@@ -23,6 +33,7 @@
 
 <script setup lang="ts">
 import mock from '~/assets/mock-data/mock.json'
+import type { useCardType } from '@/composables/useTypes'
 onMounted(() => {
   document.body.className = "bg-black font-sans";
 });
@@ -46,18 +57,34 @@ watch(
 function removeSelectedFriend(id: string){
   selectedFriends.value.delete(id)
 }
+
+const selectedFilterType = ref<string>('OR');
 const computedAlbums = computed(() => {
   if(selectedFriends.value.size === 0){
     return mock.albums
   }
-  return mock.albums.filter(album => {
-    const friends = album.ratings.friends.map(friendRating => friendRating.id)
-    for(let i = 0; i < friends.length; i++){
-      if(selectedFriends.value.has(friends[i])){
-        return true
-      }
-    }
-    return false
-  })
+  if(selectedFilterType.value === 'AND'){
+    return mock.albums.filter(album => andFriendFilter(album))
+  }
+  return mock.albums.filter(album => orFriendFilter(album))
 })
+function andFriendFilter(album: useCardType){
+  const friends = album.ratings.friends.map(friendRating => friendRating.id)
+  const selectedFriendIterator = selectedFriends.value.values()
+  for(let i = 0; i < selectedFriends.value.size; i++){
+    if(!friends.includes(selectedFriendIterator.next().value)){
+      return false
+    }
+  }
+  return true
+}
+function orFriendFilter(album: useCardType){
+  const friends = album.ratings.friends.map(friendRating => friendRating.id)
+  for(let i = 0; i < friends.length; i++){
+    if(selectedFriends.value.has(friends[i])){
+      return true
+    }
+  }
+  return false
+}
 </script>
